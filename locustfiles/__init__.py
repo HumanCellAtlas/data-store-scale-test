b42dffadaf6d5819d7c300bdaae38cc066f982b8
Trent Smith <tsmith12@ucsc.edu>
2018-05-01 15:59:16 -0700

    DSSTestClient authenticated sessions fixed

--- locustfiles/__init__.py
+++ locustfiles/__init__.py
@@ -2,4 +2,4 @@ from locustfiles.search_httplocust import SearchUser
 # from locustfiles.upload_dsslocust import UploadUser
 from locustfiles.download_dsslocust import DownloadUser
 from locustfiles.checkout_dsslocust import CheckoutUser
-# from locustfiles.notify_dsslocust import NotifiedUser
+from locustfiles.notify_dsslocust import NotifiedUser
--- locustfiles/common/dsslocust.py
+++ locustfiles/common/dsslocust.py
@@ -1,11 +1,13 @@
-from gevent import monkey
+from locust import Locust  # import first to monkey patch for green threads
 
-monkey.patch_all(thread=False)
-
+import os
 from hca.config import get_config
 from hca.dss import DSSClient
-from locust import Locust
 from locust.clients import HttpSession
+from requests_oauthlib import OAuth2Session
+
+class OAuth2SessionMod(OAuth2Session, HttpSession):
+    pass
 
 
 class DSSTestClient(DSSClient):
@@ -14,6 +16,21 @@ class DSSTestClient(DSSClient):
             self._session = HttpSession(self.host, **self._session_kwargs)
             self._session.headers.update({"User-Agent": self.__class__.__name__})
         return self._session
+
+    def get_authenticated_session(self):
+        if self._authenticated_session is None:
+            oauth2_client_data = self.application_secrets["installed"]
+            if 'GOOGLE_APPLICATION_CREDENTIALS' in os.environ:
+                token, expires_at = self._get_oauth_token_from_service_account_credentials()
+                # TODO: (akislyuk) figure out the right strategy for persisting the service account oauth2 token
+                self._authenticated_session = OAuth2SessionMod(base_url=self.host, client_id=oauth2_client_data["client_id"],
+                                                            token=dict(access_token=token),
+                                                            **self._session_kwargs)
+            else:
+                msg = ('Please set the GOOGLE_APPLICATION_CREDENTIALS environment variable')
+                raise Exception(msg.format(prog=self.__module__.replace(".", " ")))
+            self._authenticated_session.headers.update({"User-Agent": self.__class__.__name__})
+        return self._authenticated_session
 
 
 class DSSLocust(Locust):
