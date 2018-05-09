e24ae4aa47cd7fd1b8db15028d3c90dd4097a03f
Trent Smith <tsmith12@ucsc.edu>
2018-05-09 10:56:20 -0700

    updated locust_file to make customizing tests easier.

--- locustfiles/__init__.py
+++ locustfiles/__init__.py
@@ -1,7 +1,6 @@
 # from locustfiles.locustfile_example import WebsiteUser
-from locustfiles.checkout_dsslocust import CheckoutUser
-from locustfiles.download_dsslocust import DownloadUser
-from locustfiles.search_httplocust import SearchUser
-from locustfiles.notify_dsslocust import NotifiedUser
-from locustfiles.upload_dsslocust import UploadUser
-
+from locustfiles.checkout_dsslocust import CheckoutUser, CheckoutTaskSet
+from locustfiles.download_dsslocust import DownloadUser, DownloadTaskSet
+from locustfiles.search_httplocust import SearchUser, SearchTaskSet
+from locustfiles.notify_dsslocust import NotifiedUser, NotifyTaskSet
+from locustfiles.upload_dsslocust import UploadUser, UploadTaskSet
--- scripts/locust_file.py
+++ scripts/locust_file.py
@@ -1,1 +1,39 @@
-from locustfiles import UploadUser, NotifiedUser, DownloadUser, SearchUser, CheckoutUser
+from locust import HttpLocust
+
+from locustfiles.common.dsslocust import DSSLocust
+from locustfiles import UploadTaskSet, NotifyTaskSet, SearchTaskSet, DownloadTaskSet, CheckoutTaskSet
+
+
+class UploadUser(DSSLocust):
+    min_wait = 500
+    max_wait = 3000
+    task_set = UploadTaskSet
+    weight = 1
+
+
+class CheckoutUser(DSSLocust):
+    min_wait = 3000
+    max_wait = 3000
+    task_set = CheckoutTaskSet
+    weight = 3
+
+
+class DownloadUser(DSSLocust):
+    min_wait = 500
+    max_wait = 3000
+    task_set = DownloadTaskSet
+    weight = 2
+
+
+class NotifiedUser(DSSLocust):
+    min_wait = 500
+    max_wait = 3000
+    task_set = NotifyTaskSet
+    weight = 2
+
+
+class SearchUser(HttpLocust):
+    min_wait = 500
+    max_wait = 3000
+    task_set = SearchTaskSet
+    weight = 4
