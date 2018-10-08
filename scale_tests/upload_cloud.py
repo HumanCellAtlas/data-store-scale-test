11645b7348199c1049184cbf6c938b9948b7bd77
Trent Smith <tsmith12@ucsc.edu>
2018-10-08 09:21:56 -0700

    remove unused code

--- scale_tests/upload_cloud.py
+++ scale_tests/upload_cloud.py
@@ -7,14 +7,9 @@ sys.path.insert(0, pkg_root)  # noqa
 from locustfiles.common.dsslocust import DSSLocust
 from locustfiles import UploadCloudTaskSet
 
-HOST = os.getenv('TARGET_URL', "http://localhost")
-# HOST = 'https://dss.dev.data.humancellatlas.org/v1/'
-
 
 class UploadUser(DSSLocust):
     min_wait = 500
     max_wait = 500
     task_set = UploadCloudTaskSet
     weight = 1
-    # host = HOST
-
