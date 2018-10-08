e1bf3353c4517cb0678bd8c3ac2e29ae327cdcba
Trent Smith <tsmith12@ucsc.edu>
2018-10-08 09:35:45 -0700

    removed notify from __init__
    notify tests must be imported manually. This prevent the notification server from running unnecessarily.

--- locustfiles/__init__.py
+++ locustfiles/__init__.py
@@ -1,6 +1,5 @@
 from locustfiles.checkout_dsslocust import CheckoutUser, CheckoutTaskSet, CheckoutFixedTaskSet
 from locustfiles.download_dsslocust import DownloadUser, DownloadTaskSet, DownloadFixedTaskSet
 from locustfiles.search_httplocust import SearchUser, SearchTaskSet
-from locustfiles.notify_dsslocust import NotifiedUser, NotifyTaskSet
 from locustfiles.upload_dsslocust import UploadUser, UploadLocalTaskSet, UploadCloudTaskSet
 from locustfiles.common.dsslocust import DSSLocust
