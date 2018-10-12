75cf8c7959d59dc95d57a554a717cda092eaeab5
Trent Smith <tsmith12@ucsc.edu>
2018-10-12 13:43:18 -0700

    lambda_locust works in lambdas now

--- lambda_locust.py
+++ lambda_locust.py
@@ -1,33 +1,28 @@
 # -*- coding: utf-8 -*-
 import os
 import sys
+import json
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
+
 pkg_root = os.path.abspath(os.path.join(os.path.dirname(__file__), "python-packages"))
 sys.path.insert(0, pkg_root)
 
-import logging
-logging.basicConfig(level=logging.DEBUG)
+from invokust.aws_lambda import get_lambda_runtime_info
+from invokust import LocustLoadTest, create_settings
 
-import json
-from invokust import create_settings, LocustLoadTest, get_lambda_runtime_info
-import locustfiles
-
-os.environ["HOME"] = "/tmp"
-os.environ["HCA_CONFIG_FILE"] = "/tmp/config.json"
-
-with open(os.environ["HCA_CONFIG_FILE"], "w") as fh:
-    fh.write(json.dumps({}))
 
 def handler(event=None, context=None):
-    logging.warning(f"event: {event}")
-    classes = event.get('classes')
-    assert isinstance(classes, list)
-    event["classes"] = [getattr(locustfiles, obj) for obj in classes]
     try:
         settings = create_settings(**event)
+        settings.no_reset_stats = True
         loadtest = LocustLoadTest(settings)
-        loadtest.run()
+        loadtest.run(timeout=event.get("timeout", 260))
+
     except Exception as e:
         logging.error("Locust exception {0}".format(repr(e)))
+
     else:
         locust_stats = loadtest.stats()
         lambda_runtime_info = get_lambda_runtime_info(context)
@@ -35,5 +30,6 @@ def handler(event=None, context=None):
         loadtest_results.update(lambda_runtime_info)
         json_results = json.dumps(loadtest_results)
 
-        logging.warning(json_results)
+        logging.info(json_results)
         return json_results
+
