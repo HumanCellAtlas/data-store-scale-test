7e2b1b51e872999c36985cc6a7a979b44ab1d984
DailyDreaming <lblauvel@ucsc.edu>
2019-05-03 16:39:14 -0700

    Get test_invokust.py working.

--- unittests/test_invokust.py
+++ unittests/test_invokust.py
@@ -1,16 +1,15 @@
 import invokust
 
 settings = invokust.create_settings(
-    locustfile='../locustfile.py',
+    locustfile='/home/quokka/data-store-scale-test/locustfile.py',
     host='https://dss.dev.data.humancellatlas.org/v1/',
-    num_requests=10,
     num_clients=1,
-    hatch_rate=1
+    hatch_rate=1,
+    run_time='30s'
     )
 
 settings.no_reset_stats = False
-settings.run_time='30s'
 loadtest = invokust.LocustLoadTest(settings)
-loadtest.run(timeout=260)
+loadtest.run()
 stats = loadtest.stats()
 print(stats)
